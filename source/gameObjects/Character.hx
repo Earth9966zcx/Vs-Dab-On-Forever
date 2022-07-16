2ed79ef8d2da64162f10279f70ae4fe9e8e30536
Pambi69420 <chachnan789@gmail.com>
2022-07-16 21:07:17 +0700

    update Character.hx

--- source/gameObjects/Character.hx
+++ source/gameObjects/Character.hx
@@ -149,10 +149,10 @@ class Character extends FNFSprite
 				tex = Paths.getSparrowAtlas('characters/dab');
 				frames = tex;
 				animation.addByPrefix('idle', 'idle', 24, false);
-				animation.addByPrefix('singUP', 'up', 24);
-				animation.addByPrefix('singRIGHT', 'right', 24);
-				animation.addByPrefix('singDOWN', 'down', 24);
-				animation.addByPrefix('singLEFT', 'left', 24);
+				animation.addByPrefix('singUP', 'up', 24, false);
+				animation.addByPrefix('singRIGHT', 'right', 24, false);
+				animation.addByPrefix('singDOWN', 'down', 24, false);
+				animation.addByPrefix('singLEFT', 'left', 24, false);
 
 				playAnim('idle');
 				
